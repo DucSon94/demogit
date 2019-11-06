e70ff990ec66a9968bcb68f1732bee6419b6ae1c
DucSon <confickers1994@gmail.com>
2019-11-07 04:17:13 +0700

    ghfgj

--- cat.js
+++ cat.js
@@ -5,8 +5,8 @@ console.log(3);
 
 console.log("an chuot");
 
-console.log("meo meo");
+console.log("meo meosfggdg");
 console.log("an thit chuot");
 console.log("an ca");
 console.log("an tom");
-console.log("an cua");
+console.log("an cuadgfdghfhggfj");
